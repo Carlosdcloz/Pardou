fd286b08d91021aa40b296b076ef726960c2452f
Carlos Del Castillo <lozanoc916@gmail.com>
2022-07-12 23:15:09 +0200

    VB 1

---
+++ scripts/writer.js
@@ -0,0 +1,24 @@
+const typed = new Typed(".typed", {
+    strings: [
+    
+    '<i class="child">Hola, soy </i>', 
+    '<i class="child">Hi, I am </i>', 
+    
+    
+   
+
+
+
+    ], 
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    // smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+    CursorChar: "|",
+    contentType: "html" 
+})
