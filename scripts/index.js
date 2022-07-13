e276c7e98fa00ca039ec9a3c7dd08bb58bb19e06
Carlos Del Castillo <lozanoc916@gmail.com>
2022-07-13 20:40:30 +0200

    OF 1.0

---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+window.sr = ScrollReveal();
+  sr.reveal('.header', {
+    duration: 2400,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.header2', {
+    duration: 2300,
+    origin: 'right',
+    distance: '-100px'
+  });
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.text-container', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container2', {
+    duration: 2500,
+    origin: 'right',
+    distance: '-400px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container-leftbox', {
+    duration: 2500,
+    origin: 'right',
+    distance: '-1px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container-leftbox2', {
+    duration: 2500,
+    origin: 'right',
+    distance: '-1px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.section', {
+    duration: 2000,
+    origin: 'top',
+    distance: '0px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.section2', {
+    duration: 2000,
+    origin: 'top',
+    distance: '0px'
+  });
+
+
+//   window.sr = ScrollReveal();
+//   sr.reveal('.container3', {
+//     duration: 2500,
+//     origin: 'bottom',
+//     distance: '-300'
+//   });
+
+
+
+
+
+//   window.sr = ScrollReveal();
+//   sr.reveal('.container', {
+//     duration: 3000,
+//     origin: 'bottom',
+//     distance: '-100px'
+//   });
+
